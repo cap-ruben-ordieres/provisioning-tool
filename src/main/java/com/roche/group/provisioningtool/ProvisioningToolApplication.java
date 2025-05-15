784092fadf114aedd26861b6ec7e7eb4051f83fd
cap-ruben-ordieres <ruben.ordieres-blanco@capgemini.com>
2025-05-15 15:55:15 +0200

    Initial commit

---
+++ src/main/java/com/roche/group/provisioningtool/ProvisioningToolApplication.java
@@ -0,0 +1,13 @@
+package com.roche.group.provisioningtool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProvisioningToolApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProvisioningToolApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/roche/group/provisioningtool/ProvisioningToolApplicationTests.java
@@ -0,0 +1,13 @@
+package com.roche.group.provisioningtool;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProvisioningToolApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
